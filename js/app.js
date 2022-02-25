47101b3ccce4401239b30fd27c2e202fba500d37
Pedro Machado <phvieiras@hotmail.com>
2022-02-25 15:01:14 -0300

    fix

--- js/app.js
+++ js/app.js
@@ -19,65 +19,10 @@ links.forEach(links => {
 
 
 
-//nav toggle button
-
 const button = document.querySelector('.nav__toggle')
 
 button.addEventListener('click', () => {
     const select = document.querySelector('.header__nav-bar--list')
-    const toggle = select.style;
-    if (toggle.display == "block") {
-        toggle.display = "none"
-    } else {
-        toggle.display = "block"
-    }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// HOVER EFFECT WITHOUT THE LINK HOVER
-
-// const svg = document.querySelectorAll('.svgIcon');
-// svg.forEach(svg => {
-//     svg.addEventListener('mouseover', () => {
-//         path = svg.contentDocument.querySelector('svg').querySelector('path');
-//         path.setAttribute('fill', '#fff')
-//     })
-// })
-
-// svg.forEach(svg => {
-//     svg.addEventListener('mouseout', () => {
-//         path = svg.contentDocument.querySelector('svg').querySelector('path');
-//         path.setAttribute('fill', '#2C7566')
-//     })
-// })
-
-
-
-// #2C7566
-//path = svg[i].contentDocument.querySelector('svg').querySelector('path');
-//path.setAttribute('fill', '#2C7566')
+    select.classList.toggle('menu-toggle');
+}
+)
